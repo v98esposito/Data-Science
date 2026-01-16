b0bcf477cc031969df9cb1a8f0294dddc488b3ff
v98esposito <v98esposito@gmail.com>
2026-01-16 15:39:44 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,27 @@
+
+# x = 30.01 #float
+# y = 10 #int
+# z = 'stringa' #string
+# v = True/False #boolean
+# a = [1,2,3,4] #list
+# b = (1,2,3) #tuple
+# c = range (1,5) #range
+# d = {1,2,3} #set
+# e = {"nome" : "Mario" , "cognome" : "Rossi", "età" : 25} #dict
+# + - / * % ** // operatori matematici
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df_voti = pd.read_csv('voti.csv')
+
+media_voti = df_voti['Voto'].mean() #esercizio 1
+print(media_voti)
+
+media_studenti= df_voti.groupby('Studente')['Voto'].mean() #esercizio 2
+print(media_studenti)
+
+marco = df_voti[df_voti['Studente'] == 'Marco'] #esercizio 3
+print(marco)
+
+df_voti.plot(king='bar', color='skyblue', x='Studente', y='Voto') #Grafica
+plt.show()
